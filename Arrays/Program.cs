16e80e5e4c56999c100e4e93327639fcb2d6e9e6
Your Name <108307350+pjduri@users.noreply.github.com>
2024-05-14 09:50:43 -0500

    arrays exercises

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -2,5 +2,16 @@
 
 //TODO: Write your code for Array Practice below:
 
-Console.WriteLine("Hello, Arrays!");
+// int[] numbers = { 1, 1, 2, 3, 5, 8 };
+// foreach (int number in numbers) 
+// {
+//     if (number % 2 > 0) Console.WriteLine(number);
+// }
 
+string greenEggsPassage = "I would not, could not, in a box. I would not, could not with a fox.  I will not eat them in a house. I will not eat them with a mouse.";
+
+string[] greenEggsWords = greenEggsPassage.Split(' ');
+Console.WriteLine(string.Join(",", greenEggsWords));
+
+string[] greenEggsSentences = greenEggsPassage.Split(". ");
+Console.WriteLine(string.Join(" | ", greenEggsSentences));
